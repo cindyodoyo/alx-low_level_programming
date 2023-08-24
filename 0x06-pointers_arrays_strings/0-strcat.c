1094b29b5b4c301194d905bac564a8fd08861bda
cindyodoyo <cindyodoyo@gmail.com>
2023-08-24 05:33:25 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,15 +12,14 @@ char *_strcat(char *dest, char *src)
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
-		i++;
+		i++
 	}
-	j = 0;
 	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
-		j++;
+		j++
 	}
-
+	
 	dest[i] = '\0';
 	return (dest);
 }
