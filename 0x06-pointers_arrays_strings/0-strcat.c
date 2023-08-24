546c40be76177f7b2c1915088b2ce3207755a4f9
cindyodoyo <cindyodoyo@gmail.com>
2023-08-24 06:47:32 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	int destlen = 0;
+	int srclen = 0;
 	int i;
-	int j;
 
 	for (i = 0; dest[i] != '\0'; i++)
-	{
-	}
+		destlen++;
 	for (j = 0; src[j] != '\0'; j++)
-	{
-		dest[i + j] = src[j];
-	}
-
-	dest[i] = '\0';
+		srclen++;
+	for (i = 0; i <=srclen; i++)
+		dest[destlen + i] = src[i];
+	dest[i + j] = '\0';
 	return (dest);
 }
