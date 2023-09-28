47b7d065aeaec64827b0f2ab558b013cf4a6eee4
cindyodoyo <cindyodoyo@gmail.com>
2023-09-28 13:54:22 -0700

    0-binary_to_unit.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,14 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _puts_recursion(char *s);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+int _atoi(const char *s);
 int _putchar(char c);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
 
 #endif
